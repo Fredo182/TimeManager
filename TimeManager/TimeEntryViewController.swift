71eebb669217a556c71ba7c08ef31ed9956e26ff
Alfredo Castillo <alfredocastillo@alfredos-macbook-air.local>
2015-08-20 22:20:17 -0500

    Custom uipickers working! Need to style them still.

--- TimeManager/TimeEntryViewController.swift
+++ TimeManager/TimeEntryViewController.swift
@@ -8,17 +8,70 @@
 
 import UIKit
 
-class TimeEntryViewController: UIViewController {
+class TimeEntryViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var hoursArray = ["1","2","3","4","5","6","7","8","9","10","11","12"]
+    var minuteArray = ["00", "06", "12", "18", "24", "30", "36", "42", "48", "54"]
+    var suffixArray = ["AM", "PM"]
+    var projectsArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        projectsArray = ["AME","SVMS", "MOB"]
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: UIPICKER DELEGATE METHODS
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        
+        if pickerView.tag == 0 || pickerView.tag == 1 {
+            return 3
+        }
+        else{
+            return  1
+        }
+        
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        if pickerView.tag == 0 || pickerView.tag == 1 {
+            if(component == 0){
+                return hoursArray.count
+            }
+            else if(component == 1){
+                return minuteArray.count
+            }
+            else{
+                return suffixArray.count
+            }
+        }
+        else{
+            return  projectsArray.count
+        }
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 0 || pickerView.tag == 1 {
+            if(component == 0){
+                return hoursArray[row]
+            }
+            else if(component == 1){
+                return minuteArray[row]
+            }
+            else{
+                return suffixArray[row]
+            }
+        }
+        else{
+            return projectsArray[row]
+        }
+
     }
     
 
