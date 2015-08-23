87b4462a47282ac34b3603b494e0bbc4f5053075
Alfredo Castillo <alfredocastillo@alfredos-macbook-air.local>
2015-08-23 01:28:08 -0500

    created new add project alert

---
+++ AddProjectAlertView.swift
@@ -0,0 +1,18 @@
+//
+//  AddProjectAlertView.swift
+//  TimeManager
+//
+//  Created by Alfredo Castillo on 8/23/15.
+//  Copyright © 2015 Alfredo Castillo. All rights reserved.
+//
+
+import UIKit
+
+class AddProjectAlertView: NibDesignable {
+
+
+    @IBOutlet var projectName: UITextField!
+    @IBOutlet var chargeCode: UITextField!
+    @IBOutlet var cancelButton: UIButton!
+    @IBOutlet var doneButton: UIButton!
+}
